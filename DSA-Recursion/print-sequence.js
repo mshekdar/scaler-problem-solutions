b800c7673b92bd307c0e71f5a86d5a4e55bd683c
Mayuresh Shekdar <shekdarm@gmail.com>
2023-01-20 04:14:15 +0000

    Sequence printing

---
+++ DSA-Recursion/print-sequence.js
@@ -0,0 +1,19 @@
+const asc = (N) => {
+  if (N === 1) {
+    console.log(N);
+    return;
+  }
+  asc(N - 1);
+  console.log(N);
+};
+
+const desc = (N) => {
+  if (N === 0) {
+    return;
+  }
+  console.log(N);
+  desc(N - 1);
+};
+
+console.log(asc(4));
+console.log(desc(4));
