8005db8fd733a9557beab2b845b1d4957a87b4dc
mshekdar <shekdarm@gmail.com>
2023-03-26 16:33:20 +0530

    updates

--- DSA-Trees/classNotes.js
+++ DSA-Trees/classNotes.js
@@ -1,4 +1,4 @@
-// Preorder
+// Pre-order
 
 const test = {
   v: 1,
@@ -29,12 +29,52 @@ const test = {
     },
   },
 }
+//      1
+//  2       5
+//3   4   6   7
 
-preOrder(test)
+// preOrder(test)
+// postOrder(test)
+// inOrder(test)
 
+// VLR
 function preOrder(node) {
-  if (node == null) return
+  if (!node) return
   console.log(node?.v)
   preOrder(node?.l)
   preOrder(node?.r)
+}
+
+// LRV
+function postOrder(node) {
+  if (!node) return
+  postOrder(node?.l)
+  postOrder(node?.r)
+  console.log(node?.v)
+}
+
+// LVR
+function inOrder(node) {
+  if (!node) return
+  inOrder(node?.l)
+  console.log(node?.v)
+  inOrder(node?.r)
+}
+
+// console.log(size(test))
+function size(node) {
+  if (!node) return 0
+  return size(node?.l) + size(node?.r) + 1
+}
+
+// console.log(sum(test))
+function sum(node) {
+  if (!node) return 0
+  return sum(node?.l) + sum(node?.r) + node?.v
+}
+
+console.log(height(test));
+function height(node) {
+  if(!node) return -1
+  return 1 + Math.max(height(node?.l), height(node?.r))
 }
