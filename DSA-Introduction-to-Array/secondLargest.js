1eb98c24778fd049a150eacac6f92fa36960b20c
Mayuresh Shekdar <shekdarm@gmail.com>
2023-06-12 04:16:52 +0000

    formatted

--- DSA-Introduction-to-Array/secondLargest.js
+++ DSA-Introduction-to-Array/secondLargest.js
@@ -1,9 +1,8 @@
-function solve (A) {
-    const unique = new Set(A);
-    const uniqueArray = [...unique];
-    if (uniqueArray.length === 1) return -1
-    return uniqueArray.map(a => Number.parseInt(a)).sort((a, b) => b - a)[1]
+function solve(A) {
+  const unique = new Set(A)
+  const uniqueArray = [...unique]
+  if (uniqueArray.length === 1) return -1
+  return uniqueArray.map((a) => Number.parseInt(a)).sort((a, b) => b - a)[1]
 }
 
-console.log(solve([1, 2, 3, 2, 3]));
-
+console.log(solve([1, 2, 3, 2, 3]))
